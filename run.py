67a0964251d8ab9b0d6256bb5009ef5b460dbf88
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-31 15:17:11 +0800

    Add main function with terminal menu

--- run.py
+++ run.py
@@ -15,11 +15,26 @@ def play():
         print('you lost')
 
 def is_win(player, comp):
-  #returns true if the player is a winner
-  if (player == 'r' and comp == 's') or (player == 's' and comp == 'p') or (player == 'p' and comp == 'r'):
-          return True
+    #returns true if the player is a winner
+    if (player == 'r' and comp == 's') or (player == 's' and comp == 'p') or (player == 'p' and comp == 'r'):
+        return True
 
 
 def main():
+    # Clears the terminal
+    os.system('cls' if os.name == 'nt' else 'clear')
+    # Shows welcoming message
+    print('Welcome to Rock-Scissors-Paper')
+    # Presents options for the user
+    options = ['1. Yes', '2. Quit']
+    main_menu = TerminalMenu(options)
+    quitting = False
+    while quitting is not True:
+        options_index = main_menu.show()
+        options_choice = options[options_index]
+        if options_choice == '2. Quit':
+            quitting = True
+        else:
+            play()
 
-play()
+main()
