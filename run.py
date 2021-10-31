221488c13dd98121cbaf44d4fab8d450b7bd09fa
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-31 15:04:50 +0800

    Change return statements to print

--- run.py
+++ run.py
@@ -3,17 +3,16 @@ import random
 def play():
     user = input("Choose 'r' for Rock\nChoose 's' for Scissors\nChoose 'p' for Paper\n").lower()
     computer = random.choice(['r', 'p', 's'])
-
     if user == computer:
-        return 'tie'
-    if is_win(user, computer):
-        return 'you won'
-    return 'you lost'
+        print('tie')
+    elif is_win(user, computer):
+        print('you won')
+    else:
+        print('you lost')
 
 def is_win(player, comp):
   #returns true if the player is a winner
   if (player == 'r' and comp == 's') or (player == 's' and comp == 'p') or (player == 'p' and comp == 'r'):
           return True
- 
 
 play()
