20082d83c8f1efe404f3a6945bdf6717668bfbfa
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-31 14:56:40 +0800

    Create conditions for the main function

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 import random
 
 def play():
-  user = input("Choose 'r' for Rock\nChoose 's' for Scissors\nChoose 'p' for Paper")
-  computer = random.choice(['r', 'p', 's'])
+    user = input("Choose 'r' for Rock\nChoose 's' for Scissors\nChoose 'p' for Paper").lower()
+    computer = random.choice(['r', 'p', 's'])
 
-  
+    if user == computer:
+        return 'tie'
+    if is_win(user, computer):
+        return 'you won'
+    return 'you lost'
+
